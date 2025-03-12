b55b6e308fa9113b57053bd8dcd91792f2592578
ViktorSvertoka <victor.svertoka@gmail.com>
2025-03-12 22:09:38 +0200

    feat(JS): add task 2

--- task2.js
+++ task2.js
@@ -2,6 +2,18 @@
 //         1) 1-ша кнопка – при кліку на неї колір фону сторінки міняється на синій
 //         2) 2-га кнопка – при подвійному кліку на неї колір фону сторінки міняється на рожевий
 //         3) 3-я кнопка – при наведенні колір фону міняється на коричневий
-//         
-// Запустіть виконане завдання за допомогою Live Server 
+//
+// Запустіть виконане завдання за допомогою Live Server
 // Перевірте виконання за допомогою команди node tests/task2.test.js
+
+document.querySelector("#button1").addEventListener("click", () => {
+  document.body.style.backgroundColor = "blue";
+});
+
+document.querySelector("#button2").addEventListener("dblclick", () => {
+  document.body.style.backgroundColor = "pink";
+});
+
+document.querySelector("#button3").addEventListener("mouseover", () => {
+  document.body.style.backgroundColor = "brown";
+});
