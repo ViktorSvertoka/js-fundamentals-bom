0e81d8c38d59922db47557639af2b39333e618ff
ViktorSvertoka <victor.svertoka@gmail.com>
2025-03-12 22:18:35 +0200

    feat(JS): add task 4

--- task4.js
+++ task4.js
@@ -2,3 +2,12 @@
 //  і виводитиме на поточну сторінку при її розтязі/стисканні відповідні значення.
 // Для запуску використовувати Live Server
 // Для перевірки node tests/task4.test.js
+
+function updateWindowSize() {
+  document.querySelector("#widthValue").textContent = window.innerWidth;
+  document.querySelector("#heightValue").textContent = window.innerHeight;
+}
+
+window.addEventListener("resize", updateWindowSize);
+
+updateWindowSize();
