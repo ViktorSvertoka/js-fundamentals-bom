a000d3193b1820749b73343fafe1d40a5612c5fd
ViktorSvertoka <victor.svertoka@gmail.com>
2025-03-12 22:13:12 +0200

    feat(JS): add task 3

--- task3.js
+++ task3.js
@@ -1,5 +1,14 @@
-// Реалізуйте програму, яка по натисканню на кнопку видалятиме обраний елемент випадаючого списку. 
+// Реалізуйте програму, яка по натисканню на кнопку видалятиме обраний елемент випадаючого списку.
 // Можуть видалятися всі елементи в будь-якому порядку.
 // Список з елементами знаходиться у файлі index.html
 // Для запуску використовувати Live Server
 // Для перевірки виконання виконати команду node tests/task3.test.js
+
+document.querySelector("#deleteButton").addEventListener("click", () => {
+  const dropdown = document.querySelector("#dropdown");
+  const selectedOption = dropdown.options[dropdown.selectedIndex];
+
+  if (selectedOption) {
+    selectedOption.remove();
+  }
+});
