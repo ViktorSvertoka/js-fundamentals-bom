2dfa53b6b2f6c5779fd7fc4dfc939f2520eafb56
ViktorSvertoka <victor.svertoka@gmail.com>
2025-03-12 22:19:47 +0200

    feat(JS): change test

--- tests/task4.test.js
+++ tests/task4.test.js
@@ -1,13 +1,13 @@
-const puppeteer = require('puppeteer');
+const puppeteer = require("puppeteer");
 
 (async () => {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({ headless: "new" });
   const page = await browser.newPage();
 
-  await page.goto('http://127.0.0.1:5500/index.html');
+  await page.goto("http://127.0.0.1:5500/index.html");
 
-  const widthSelector = '#widthValue';
-  const heightSelector = '#heightValue';
+  const widthSelector = "#widthValue";
+  const heightSelector = "#heightValue";
 
   async function getTextFromSelector(selector) {
     return await page.evaluate((sel) => {
@@ -15,7 +15,6 @@ const puppeteer = require('puppeteer');
       return element ? element.textContent : null;
     }, selector);
   }
-
 
   const initialWidth = await getTextFromSelector(widthSelector);
   const initialHeight = await getTextFromSelector(heightSelector);
@@ -26,13 +25,15 @@ const puppeteer = require('puppeteer');
   const updatedWidth = await getTextFromSelector(widthSelector);
   const updatedHeight = await getTextFromSelector(heightSelector);
 
-  console.log('Updated Width:', updatedWidth);
-console.log('Updated Height:', updatedHeight);
+  console.log("Updated Width:", updatedWidth);
+  console.log("Updated Height:", updatedHeight);
 
   if (initialWidth !== updatedWidth || initialHeight !== updatedHeight) {
-    console.log('Window size changed successfully.');
+    console.log("Window size changed successfully.");
   } else {
-    console.log('Error: Window size was not changed or values are not displayed.');
+    console.log(
+      "Error: Window size was not changed or values are not displayed."
+    );
   }
 
   await browser.close();
